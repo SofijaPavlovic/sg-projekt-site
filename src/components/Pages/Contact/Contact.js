2ebc7ee075f5b11a45f79fbc3321d3efaa56a4c7
Sofija Pavlovic <sofija.pavlovic.etf@gmail.com>
2020-04-25 18:15:40 +0200

    font styles, contact page

--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -4,18 +4,20 @@ import ContactInformations from './ContactInformation/ContactInformation';
 
 const contact = (props) => (
     <div className="Contact">
-        <div className="Informations">
-            <h1>Kontakt informacije</h1>
-            <ContactInformations info={props.info} />
-        </div>
-
-        <div className="Map" >
+        <h2>Kontakt informacije</h2>
+        <div className="BreakLine" />
+        <div className="Informations">  
+            <div className="Info">
+            <ContactInformations className="Info" info={props.info} />
+            </div>
+            <div className="Map" >
             <iframe title="Contacts map"
                 width="100%"
                 height="100%"
                 src="https://maps.google.com/maps?q=Pavla%20%C5%A0tosa%205%2C%20Subotica&t=&z=16&ie=UTF8&iwloc=&output=embed"
                 frameBorder="0">
             </iframe> 
+        </div>
         </div>
     </div >
 )
